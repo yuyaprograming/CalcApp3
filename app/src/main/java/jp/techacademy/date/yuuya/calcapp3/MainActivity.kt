a67711f702fee5ac9a9367e4963fea7b14a20168
yuyaprograming <ienjoyschoollife@gmail.com>
2020-08-02 00:10:01 +0900

    Snackbarのエラー解消

--- app/src/main/java/jp/techacademy/date/yuuya/calcapp3/MainActivity.kt
+++ app/src/main/java/jp/techacademy/date/yuuya/calcapp3/MainActivity.kt
@@ -7,7 +7,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +23,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onClick(v: View) {
+        val intent = Intent(this, SecondActivity::class.java)
         var edittext1 = editText1.text.toString()
         var edittext2 = editText2.text.toString()
+        edittext1.toDouble()
+
+        fun edittext1.toDouble()
+
         if (v.id == R.id.button1) {
             intent.putExtra("VALUE1",edittext1+edittext2)
         } else if (v.id == R.id.button2) {
@@ -34,12 +39,6 @@ class MainActivity : AppCompatActivity() {
         } else if (v.id == R.id.button4) {
             intent.putExtra("VALUE3",edittext1/edittext2)
         }
-    }
-    override fun onClick(v: View?) {
-        val intent = Intent(this, SecondActivity::class.java)
-        intent.putExtra("VALUE1", 10)
-        intent.putExtra("VALUE2", 20)
         startActivity(intent)
     }
-
 }
